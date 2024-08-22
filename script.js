e70c65bd15adad9e5e1bc93807e417bde96eebaf
Lukeneas <lcs.ysh@hotmail.com>
2024-08-22 02:09:07 -0300

    alteracoes no modal

--- script.js
+++ script.js
@@ -4,15 +4,16 @@
 //3 - joga na tela
 
 //INITIAL DATA
+let modalQt = 1;
 const c = (el) => document.querySelector(el);
-const cs = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((pizza, index) => {
   //clones dos modelos html
   let pizzaItem = c(".models .pizza-item").cloneNode(true);
 
   //armazenar chave em cada pizza
-  pizzaItem.setAttribute('data-key', index);  
+  pizzaItem.setAttribute("data-key", index);
   pizzaItem.querySelector(".pizza-item--img img").src = pizza.img;
   pizzaItem.querySelector(".pizza-item--name").innerHTML = pizza.name;
   pizzaItem.querySelector(
@@ -22,13 +23,29 @@ pizzaJson.map((pizza, index) => {
   //evento de pizza clicada
   pizzaItem.querySelector("a").addEventListener("click", (e) => {
     e.preventDefault();
+    //sempre que abrir modal, zera qt
+    modalQt = 1;
     //closest procura el mais proximo com essa class
-    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
 
-    c('.pizzaBig img').src = pizzaJson[key].img;
-    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    c(".pizzaBig img").src = pizzaJson[key].img;
+    c(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description;
+    c(".pizzaInfo--actualPrice").innerHTML = pizzaJson[key].price.toFixed(2);
+    //REMOVE PRE-SELECTED caso a pessoa clique em um tamanho e pule para outra pizza
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    cs(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      //sempre que abrir uma pizza, grande será o tamanho PRE-SELECTED
+      if(sizeIndex == 2) {
+        size.classList.add('selected');
+      };
+      //pizza clicada //medidas //cada medida jogada dentro de span conectando sizeIndex(n sizes), consequentemente jogando em n spans que estao dentro das sizes
+      size.querySelector('span').innerHTML = 
+      pizzaJson[key].sizes[sizeIndex];;
+    });
 
+    //quantidade pizza
+    c('.pizzaInfo--qt').innerHTML = modalQt;
     //animação modal
     c(".pizzaWindowArea").style.opacity = 0;
     c(".pizzaWindowArea").style.display = "flex";
